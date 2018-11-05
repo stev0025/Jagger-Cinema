3e9ffd3438b4f60cccf2ae87d2cb8987ed265807
stev0025 <37723560+stev0025@users.noreply.github.com>
2018-11-05 14:16:51 +0800

    Update SQL folder
    add a single imported file

--- sql/seed_order.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO orders(id,title,email,seat) VALUES (1,'MOVIE','applewoman@gmail.com','TITLE_DAY_TIMING_SEATNUMBER');
-INSERT INTO orders(id,title,email,seat) VALUES (2,'MOVIE','applewoman@gmail.com','2');
-INSERT INTO orders(id,title,email,seat) VALUES (3,'MOVIE','applewoman@gmail.com','3');
